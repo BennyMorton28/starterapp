714f6dea94896d571df42e95b3fa043ffa521168
Ben Morton <ben.morton@kellogg.northwestern.edu>
2024-03-05 18:27:43 +0000

    added number display and simple logic

---
+++ app/controllers/blackjack_controller.rb
@@ -0,0 +1,44 @@
+class BlackjackController < ApplicationController
+  def index
+    if params[:player_card_1]
+      player_hand = [params[:player_card_1], params[:player_card_2]]
+      dealer_card = params[:dealer_card]
+      @advice = decide_action(player_hand, dealer_card)
+    end
+  end
+
+  private
+
+  def decide_action(player_hand, dealer_card)
+    player_total = calculate_total(player_hand)
+    dealer_total = card_value(dealer_card)
+
+    if player_total <= 11
+      'Hit'
+    elsif player_total >= 17
+      'Stand'
+    elsif player_total > 11 && player_total < 17 && dealer_total < 7
+      'Stand'
+    else
+      'Hit'
+    end
+  end
+
+  def calculate_total(hand)
+    total = 0
+    hand.each do |card|
+      total += card_value(card)
+    end
+    total
+  end
+
+  def card_value(card)
+    if card.match?(/\d/)
+      card.to_i
+    elsif ['J', 'Q', 'K'].include?(card)
+      10
+    else # Ace
+      11
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'blackjack/index'
   # Resources
   resources "sessions"
   resources "users"
@@ -9,4 +10,10 @@ Rails.application.routes.draw do
 
   # Define the root route
   get("/", { :controller => "users", :action => "new" })
+
+  Rails.application.routes.draw do
+    # Define a route for the Blackjack form submission
+    get 'blackjack', to: 'blackjack#index', as: 'blackjack'
+    # This creates a path helper `blackjack_path` that routes to BlackjackController#index
+  end
 end
